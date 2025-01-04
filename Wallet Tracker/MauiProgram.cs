58ac5fc832538bb085854919fff367292b3c4165
aayushbhattarai7 <aayush.bhattarai.np@gmail.com>
2025-01-04 22:27:20 +0545

    added files

---
+++ Wallet Tracker/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Wallet_Tracker
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            // Register UserService for dependency injection
+            builder.Services.AddScoped<UserService>();
+            builder.Services.AddScoped<TransactionService>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Wallet Tracker/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet_Tracker.Models
+{
+    public class User
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+
+        public int Credit { get; set; } = 0;
+
+        public int Debit { get; set; } = 0;
+
+        public int Debt { get; set; } = 0;
+
+        public int TotalDebt { get; set; } = 0;
+
+        public int DebtTaken { get; set; } = 0;
+
+        public int ClearedDebt { get; set; } = 0;
+
+        
+
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+    }
+}
