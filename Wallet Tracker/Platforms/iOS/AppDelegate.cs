0d9c3b458437dbd6d1481ee4de07ef2c7ba39c72
aayushbhattarai7 <aayush.bhattarai.np@gmail.com>
2025-01-04 22:27:20 +0545

    added files

---
+++ Wallet Tracker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Wallet_Tracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
